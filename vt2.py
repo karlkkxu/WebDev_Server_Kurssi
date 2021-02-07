f74e28dcfe773cf83d747c274c4a26f666685e48
karlkkxu <kim.karlsson@outlook.com>
2021-02-07 19:23:23 +0200

    tp

--- vt2.py
+++ vt2.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import Form, BooleanField, StringField, validators, IntegerField, SelectField, widgets, SelectMultipleField, ValidationError
-
-class Arvosanalaskuri(FlaskForm):
-    etunimi     = StringField('Etunimi')
-    sukunimi    = StringField('Sukunimi')
-
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
-@app.route('/wtlomake', methods=['POST','GET']) 
-def wtlomake():
-    form = Arvosanalaskuri(csrf_enabled=False)
-    return render_template('wtlomake.html', form=form)
+@app.route('/', methods=['POST','GET']) 
+def peliLauta():
+    p1 = " "
+    p2 = " "
+    koko = 8
+
+    try:
+        p1 = str(request.form.get("p1") or " ")
+    except:
+        p1 = "VITUN LAITON"
+
+    try:
+        p2 = str(request.form.get("p2") or " ")
+    except:
+        p2 = "VITUN LAITON"
+
+    try:
+        koko = int(request.form.get("koko") or 8)
+    except:
+        koko = 9999999
+
+    return render_template("jinja.xhtml", koko=koko, p1=p1, p2=p2)
